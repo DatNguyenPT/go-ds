021154f89e4f18951ac8d3fbba8e0b060d1024d1
Dat Nguyen <datnguyen@onhandbi.com>
2025-05-14 10:41:00 +0700

    pair

---
+++ pair/pair.go
@@ -0,0 +1,41 @@
+package pair
+
+import "fmt"
+
+type Pair struct {
+	a, b any // No need for pointers
+}
+
+// GetFirst returns the first value if present, otherwise an error.
+func (pair *Pair) GetFirst() (any, error) {
+	if pair.a != nil {
+		return pair.a, nil
+	}
+	return nil, fmt.Errorf("there is no first value")
+}
+
+// GetSecond returns the second value if present, otherwise an error.
+func (pair *Pair) GetSecond() (any, error) {
+	if pair.b != nil {
+		return pair.b, nil
+	}
+	return nil, fmt.Errorf("there is no second value")
+}
+
+// RemoveFirst removes the first value if present.
+func (pair *Pair) RemoveFirst() (bool, error) {
+	if pair.a != nil {
+		pair.a = nil
+		return true, nil
+	}
+	return false, fmt.Errorf("no first element exists")
+}
+
+// RemoveSecond removes the second value if present.
+func (pair *Pair) RemoveSecond() (bool, error) {
+	if pair.b != nil {
+		pair.b = nil
+		return true, nil
+	}
+	return false, fmt.Errorf("no second element exists")
+}
